71569e9a238a7eb8f9972071bfd98d19a4bef231
Berthy Feng <berthyf96@gmail.com>
2022-02-05 15:48:51 -0800

    lighslider for project highlights slideshow

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -97,4 +97,14 @@ $(document).ready(function(){
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
 
+  // lightSlider project slideshow
+  $("#light-slider").lightSlider({
+    item: 4,
+    loop: true,
+    controls: false,
+    auto: true,
+    pauseOnHover: true,
+    pause: 3000
+  });
+
 });
